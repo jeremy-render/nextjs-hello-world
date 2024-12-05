6ff69ac63bd00ddd198072a27d9efc41ba524a13
Jérémy Faret <jeremy@render.com>
2024-12-05 16:49:59 +0200

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
 	return (
 		<div>
-			<h1>Hello from Next!!</h1>
+			<h1>Hello from Next!!:::::</h1>
 			{instanceInfo && (
 				<pre>{JSON.stringify(instanceInfo, null, 2)}</pre>
 			)}
